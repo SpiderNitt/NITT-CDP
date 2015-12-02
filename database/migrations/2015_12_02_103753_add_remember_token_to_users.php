367464e745d6adbe278f2c10bb4aa5220ba0451a
Siddarth Iyer <mindstormer619@gmail.com>
2015-12-02 16:09:21 +0530

    [MIGRATION] Add remember token to user table
    Remember to rollback before switching out of this branch.
    Migration completed.

---
+++ database/migrations/2015_12_02_103753_add_remember_token_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRememberTokenToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('remember_token')->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('remember_token');
+        });
+    }
+}
