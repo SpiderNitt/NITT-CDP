fc21315dd501d1e18fa84774eabbf93339bb47c9
Siddarth Iyer <mindstormer619@gmail.com>
2016-01-30 18:46:53 +0530

    [MIG] Migration for Subscriptions

---
+++ database/migrations/2016_01_30_131150_create_subscriptions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->morphs('subscribable');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->unique(['user_id', 'subscribable_id', 'subscribable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('subscriptions');
+    }
+}
